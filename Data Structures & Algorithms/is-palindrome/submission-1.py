91b2a7d547ea0ec40a762e2306b40864147b1133
Soham Patel <35080866+ssp7@users.noreply.github.com>
2026-07-11 20:10:18 +0000

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sanitized = ""
+        for c in s:
+            if c.isalnum():
+                sanitized += c
+        
+        l, r = 0, len(sanitized) - 1
+        while l < r:
+            if sanitized[l].lower() != sanitized[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
